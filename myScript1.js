3157fe0d07d460be094bc3f424163ac2de3cc9d8
EcclesiasteJ <99517476+EcclesiasteJ@users.noreply.github.com>
2022-03-15 07:07:17 -0300

    Create myScript1.js
    .js

---
+++ myScript1.js
@@ -0,0 +1,6 @@
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
+function function1() {
+  document.getElementById("demo").innerHTML = "Another Paragraph.";
+}
